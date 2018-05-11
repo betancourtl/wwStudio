38ae051e05833f032b0ee9391e21ca9b79579e8f
Luis Betancourt <webdeveloperpr@gmail.com>
2018-05-10 21:15:00 -0400

    Add build version

--- config/loaders/css.js
+++ config/loaders/css.js
@@ -1,14 +1,20 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const extractCSS = new ExtractTextPlugin('styles.css');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const extractCSS = new MiniCssExtractPlugin({
+  // Options similar to the same options in webpackOptions.output
+  // both options are optional
+  filename: "[name].css",
+  chunkFilename: "[id].css"
+});
 
 const cssLoaderProd = [
   {
-    test: /\.css$/,
-    use: extractCSS.extract(['css-loader'])
-  },
-  {
-    test: /\.scss$/i,
-    use: extractCSS.extract(['css-loader', 'sass-loader'])
+    test: /\.s?[ac]ss$/,
+    use: [
+      MiniCssExtractPlugin.loader,
+      'css-loader',
+      'sass-loader',
+    ]
   },
 ];
 
@@ -22,6 +28,7 @@ const cssLoaderDev = [
     use: ['style-loader', 'css-loader', 'sass-loader'],
   },
 ];
+
 
 module.exports = {
   prod: {
